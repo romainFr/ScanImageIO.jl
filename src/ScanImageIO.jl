67ab377f2f3828ed05e989531519c1e212679913
Romain Franconville <romain.franconville@protonmail.com>
2019-04-19 17:53:43 -0400

    Functional loading of multiple movies.

--- src/ScanImageIO.jl
+++ src/ScanImageIO.jl
@@ -6,18 +6,39 @@ function read_movie_set(files::Array{String,1};binFile=nothing,rois=nothing,chan
 
     
     @info "Reading metadata" ## First need to check that the movies have compatible sizes
-    fullMeta = [ScanImageTiffReader.open(io -> parse_SI_meta(ScanImageTiffReader.metadata(io)),f) for f in files]
-    fullSz = [ScanImageTiffReader.open(io -> ScanImageTiffReader.size(io),f) for f in files]
-    px = ScanImageTiffReader.open(io -> ScanImageTiffReader.size(io),files[1])
+    fullMeta = map(files) do f
+        ScanImageTiffReader.open(f) do io
+            parse_SI_meta(ScanImageTiffReader.metadata(io))
+        end
+    end
     
+    fullSz = map(files) do f
+        ScanImageTiffReader.open(f) do io
+            ScanImageTiffReader.size(io)
+        end
+    end
+
+    fullFirstFrame = map(files) do f
+        ScanImageTiffReader.open(f) do io
+            parse_SI_descr(ScanImageTiffReader.description(io,1))
+        end
+    end
+    
+    px =  ScanImageTiffReader.open(files[1]) do io
+        ScanImageTiffReader.pxtype(io)
+    end
+    @info px
+    
+    @info "Check the compatibility of the movie set"
     @assert all([f[1:2] == fullSz[1][1:2] for f in fullSz]) "Frames do not have the same size"
     @assert all([frameRate(fm) == frameRate(fullMeta[1]) for fm in fullMeta]) "Not the same frame rate"
     @assert all([hasfastz(fm) == hasfastz(fullMeta[1]) for fm in fullMeta])  "Not all movies are volumetric (or non volumetric)"
     @assert all([volumeRate(fm) == volumeRate(fullMeta[1]) for fm in fullMeta]) "Not the same volume rate"
     @assert all([nrois(fm) == nrois(fullMeta[1]) for fm in fullMeta]) "Not the same ROI structure"
     @assert all([savedChannels(fm) == savedChannels(fullMeta[1]) for fm in fullMeta]) "Not the same channels acquired"
-    
-    movieSizes = [acquired_channel_frame_slice_volume(fm,fullSz[1]) for fm in fullMeta]
+
+    @info "Gather movie sizes"
+    movieSizes = [acquired_channel_frame_slice_volume(fullMeta[i],fullSz[i]) for i in 1:length(fullMeta)]
     sz = fullSz[1][1:2]
     nChannels = movieSizes[1][1]
     nFrames = [m[2] for m in movieSizes]
@@ -28,7 +49,7 @@ function read_movie_set(files::Array{String,1};binFile=nothing,rois=nothing,chan
     channels = isnothing(channels) ? (1:nChannels) : channels
     frames = [isnothing(frames) ? (1:nFrames[i]) : frames for i in 1:length(movieSizes)]
     slices = [isnothing(slices) ? (1:nRealSlices[i]) : slices for i in 1:length(movieSizes)]
-    volumes = [isnothing(volumes) ? (1:nVolumes[i]) : volumes i in 1:length(movieSizes)]
+    volumes = [isnothing(volumes) ? (1:nVolumes[i]) : volumes for i in 1:length(movieSizes)]
     nRois = nrois(fullMeta[1])
     rois = isnothing(rois) ? (1:nRois) : rois
 
@@ -47,7 +68,7 @@ function read_movie_set(files::Array{String,1};binFile=nothing,rois=nothing,chan
         end
     end
     
-    dataLengths = [prod([sz... ,nChannels[i],nFrames[i],nAcquiredSlices[i],nVolumes[i],nRois]) for i in 1:length(fullMeta)]
+    dataLengths = [prod([sz... ,nChannels,nFrames[i],nAcquiredSlices[i],nVolumes[i],nRois]) for i in 1:length(fullMeta)]
     
     if hasmRoi(fullMeta[1])
         linesBetweenScanFields = nlinesBetweenFields(fullMeta[1]) 
@@ -64,7 +85,8 @@ function read_movie_set(files::Array{String,1};binFile=nothing,rois=nothing,chan
     end
 
     dataOut = Array{SharedArray}(undef,length(rois))
-    
+
+    @info "Creating shared arrays"
     i = 1
     if isnothing(binFile)
         for r in rois
@@ -80,15 +102,16 @@ function read_movie_set(files::Array{String,1};binFile=nothing,rois=nothing,chan
         end
     end
     
-    movingDim = (frames,slices,volumes)[fullD-3]
-    offsetMoving = [1;cumsum([length(mD) for mD in movingDim])...]
+    movingDim = (frames,slices,volumes)[fullDims-3]
+    offsetMoving = [0;cumsum([length(mD) for mD in movingDim])...]
+    @info "Write movie data"
     @sync begin
         for p in procs(dataOut[1])          
-            @async remotecall_wait(write_movie_toshared_native_chunk, p, dataOut,files,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,channels,frames,slices,volumes,rois,offsetMoving)
+            @async remotecall_wait(write_movie_toshared_chunk, p, dataOut,files,sz,nChannels,nFrames,nAcquiredSlices,nVolumes,fullDims,dataLengths,channels,frames,slices,volumes,rois,offsetMoving,roiPos)
         end
     end
     
-    dataOut
+    dataOut,fullFirstFrame,nVolumes
     
 end
 
@@ -127,18 +150,18 @@ function acquired_channel_frame_slice_volume(SImeta,sz)
 end
 
 ## Read a SI movie and return an array of SharedArrays
-function write_movie_toshared_chunk(out,files,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,dataLength,channels,frames,slices,volumes,rois,startPoints,offsets)
-   write_movie_toshared_native_chunk(out,files,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,dataLength,channels,frames,slices,volumes,rois,offsets,myrange(out[1],[0,length(files)]))
+function write_movie_toshared_chunk(out,files,sz,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,dataLength,channels,frames,slices,volumes,rois,offsets,roisPos)
+   write_movie_toshared_chunk(out,files,sz,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,dataLength,channels,frames,slices,volumes,rois,offsets,roisPos,myrange(out[1],[0,length(files)]))
 end
 
-function write_movie_toshared_chunk(out,files,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,dataLength,channels,frames,slices,volumes,rois,offsets,filerange)
+function write_movie_toshared_chunk(out,files,sz,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,dataLength,channels,frames,slices,volumes,rois,offsets,roisPos,filerange)
     for i in filerange
         
-        write_movie_to_shared(out,files[i],nChannels,nFrames[i],nAcquiredSlices[i],nVolumes[i],fullD,dataLength[i],channels,frames[i],slices[i],volumes[i],rois,offsets[i])
+        write_movie_to_shared(out,files[i],sz,nChannels,nFrames[i],nAcquiredSlices[i],nVolumes[i],fullD,dataLength[i],channels,frames[i],slices[i],volumes[i],rois,offsets[i],roisPos)
     end
 end
 
-function write_movie_to_shared(out,f::String,sz,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,dataLength,channels,frames,slices,volumes,rois,offset)
+function write_movie_to_shared(out,f::String,sz,nChannels,nFrames,nAcquiredSlices,nVolumes,fullD,dataLength,channels,frames,slices,volumes,rois,offset,roisPos)
     data = ScanImageTiffReader.open(f) do io
         ScanImageTiffReader.data(io)
     end
@@ -147,9 +170,9 @@ function write_movie_to_shared(out,f::String,sz,nChannels,nFrames,nAcquiredSlice
         data = reshape(data[1:dataLength],(sz[1],sz[2],nChannels,nFrames,nAcquiredSlices,nVolumes))
         data = permutedims(data,(2,1,3,4,5,6))
 
-        linearOffset = 1 + (offset-1) * stride(out[r],fullD)
+        linearOffset = 1 + offset * stride(out[r],fullD)
         outLength = prod([roisPos[r],sz[1],length(channels),length(frames),length(slices),length(volumes)])
-        out[r][linearOffset:(linearOffset+outLength)] = data[roisPos[r],:,channels,frames,slices,volumes]
+        out[r][linearOffset:(linearOffset+outLength-1)] = data[roisPos[r],:,channels,frames,slices,volumes]
     end
     out
 end
@@ -163,9 +186,6 @@ function myrange(q::SharedArray,bigrange)
     splits = [round(Int, s) for s in range(bigrange[1],stop=bigrange[2],length=nchunks+1)]
     splits[idx]+1:splits[idx+1]
 end
-
-
-
 
 ## Reading the metadata header as a Dictionary, in case it's not a JSON header
 function makeLineDict(metaLine)
@@ -202,6 +222,12 @@ function parse_SI_meta(meta)
         metaP = recursiveMerge([makeLineDict(mL) for mL in metaP]...)
         return merge(metaP,rois)
     end
+end
+
+function parse_SI_descr(meta)
+    metaP = split(meta,"\n")[1:end-1]
+    metaP = merge([makeLineDict(mL) for mL in metaP]...)
+    metaP
 end
 
 end # module
